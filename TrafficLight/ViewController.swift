b59d7bcc78bff640cd15774b9ce54ec2f1019731
MiZaft <mi.zaft1@gmail.com>
2023-05-07 02:10:26 +0300

    Application is ready;

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -9,11 +9,51 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redLightView: UIView!
+    @IBOutlet var yellowLightView: UIView!
+    @IBOutlet var greenLightView: UIView!
+    
+    @IBOutlet var conditionLightButton: UIButton!
+    
+    private var activeLight = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        redLightView.alpha = 0.3
+        yellowLightView.alpha = 0.3
+        greenLightView.alpha = 0.3
+        
+        conditionLightButton.layer.cornerRadius = 10
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        redLightView.layer.cornerRadius = redLightView.layer.bounds.width / 2
+        yellowLightView.layer.cornerRadius = yellowLightView.frame.size.width / 2
+        greenLightView.layer.cornerRadius = greenLightView.frame.size.width / 2
     }
 
-
+    @IBAction func conditionLightButtonDidTapped() {
+        if conditionLightButton.currentTitle == "START" {
+            conditionLightButton.setTitle("NEXT", for: .normal)
+        }
+        
+        switch activeLight {
+        case "yellow":
+            activeLight = "green"
+            yellowLightView.alpha = 0.3
+            greenLightView.alpha = 1
+        case "red":
+            activeLight = "yellow"
+            redLightView.alpha = 0.3
+            yellowLightView.alpha = 1
+        default:
+            activeLight = "red"
+            redLightView.alpha = 1
+            greenLightView.alpha = 0.3
+        }
+    }
 }
 
